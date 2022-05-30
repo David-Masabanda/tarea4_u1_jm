d6592156b80fa1da3fab3e7d64f7a0c13a1c229a
David-Masabanda <josue.masabanda99@gmail.com>
2022-05-30 11:33:33 -0500

    Subir deber a GitHub

---
+++ src/main/java/com/uce/edu/demo/matricula/Matricula.java
@@ -0,0 +1,24 @@
+package com.uce.edu.demo.matricula;
+
+public class Matricula {
+	
+	private Estudiante estudiante;
+	private Materia materia;
+	
+	//SET y GET
+	public Estudiante getEstudiante() {
+		return estudiante;
+	}
+	public void setEstudiante(Estudiante estudiante) {
+		this.estudiante = estudiante;
+	}
+	public Materia getMateria() {
+		return materia;
+	}
+	public void setMateria(Materia materia) {
+		this.materia = materia;
+	}
+	
+	
+	
+}
